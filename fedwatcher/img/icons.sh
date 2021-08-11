9f9c8a8d83c1e23bf93fcc5272866b21b54f8b79
matiasandina <matiasandina@gmail.com>
2021-08-10 21:17:19 -0400

    adding fedwatcher logos

---
+++ fedwatcher/img/icons.sh
@@ -0,0 +1,6 @@
+inkscape -w 16 -h 16 -o 16.png fedwatcher_logo.svg
+inkscape -w 32 -h 32 -o 32.png fedwatcher_logo.svg
+inkscape -w 48 -h 48 -o 48.png fedwatcher_logo.svg
+inkscape -w 64 -h 64 -o 64.png fedwatcher_logo.svg
+
+convert 64.png icon.ico
