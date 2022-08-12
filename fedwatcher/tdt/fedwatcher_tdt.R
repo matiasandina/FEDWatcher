5412d5f055b689e2edd78ab1945b88341ff5dd1c
Matias Andina <matiasandina@gmail.com>
2022-08-12 11:59:06 -0400

    adding function to sync TDT timestamps in R

---
+++ fedwatcher/tdt/fedwatcher_tdt.R
@@ -0,0 +1,38 @@
+# This function can be used to transform the timestamps 
+# from TDT system  
+# library(reticulate)
+# if FED connected to PtC0
+# py$block['epocs']['PtC0']['onset']
+# trial_start <- py$block["info"]['start']
+#' @param event_onset vector with onset timestamps in seconds from TDT block
+#' @return A `data.frame` with 
+#' #' \itemize{
+#'   \item event - The number of event.
+#'   \item seconds - The first recorded timestamp of the event.
+#'   \item tdt_datetime - If `trial_start` is `POSIXct`, a `POSIXct` timestamp will be calculated using the `seconds` coulmn.
+#' }
+#' 
+fedwatcher_tdt <- function(event_onset, trial_start=NULL){
+  # we believe two real events can't be 0.01 seconds apart
+  event_threshold <- 0.01
+  # diff will remove the first event, so we add it
+  # cumsum will give us clusters
+  cluster_values <- cumsum(c(1, diff(event_onset)) > event_threshold)
+  event_number <- length(unique(cluster_values))
+  # we could do a formal clustering approach 
+  # hclust works well, kmeans is unstable
+  # cutree(hclust(dist(event_onset)),k = event_number)
+  
+  event_df <- aggregate(fed3_event_onset, 
+                        by = list(event = cluster_values), 
+                        FUN = min)
+  event_df <- dplyr::rename(event_df, seconds = x)
+  
+  # Transform seconds into datetime
+  if(lubridate::is.POSIXct(trial_start)){
+    usethis::ui_info("Trial start provided at {trial_start}, adding `tdt_datetime` column.")
+    event_df$tdt_datetime <- trial_start + lubridate::seconds(event_df$seconds)
+  }
+  
+  return(event_df)
+}
